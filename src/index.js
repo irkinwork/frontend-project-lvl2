d4de60b6a5b3063d7b8ea0e6fa1eb066b18c7499
i <irin.work@gmail.com>
2019-06-26 00:35:16 +0300

    add renderList

--- src/index.js
+++ src/index.js
@@ -7,11 +7,25 @@ import renderPlainDiff from './formatters/plain';
 
 const readFile = fileConfig => fs.readFileSync(path.resolve(process.cwd(), fileConfig), 'utf-8');
 
-export default (firstConfig, secondConfig, format) => {
+const renderList = [
+  {
+    type: 'plain',
+    render: diff => renderPlainDiff(diff),
+  },
+  {
+    type: 'tree',
+    render: diff => stringify(renderDiff(diff)),
+  },
+];
+
+const getRender = format => renderList.find(({ type }) => type === format);
+
+export default (firstConfig, secondConfig, format = 'tree') => {
   const file1 = readFile(firstConfig);
   const file2 = readFile(secondConfig);
   const parsedFile1 = parsers(firstConfig, file1);
   const parsedFile2 = parsers(secondConfig, file2);
   const calculatedDiff = getDiff(parsedFile1, parsedFile2);
-  return format === 'plain' ? renderPlainDiff(calculatedDiff) : stringify(renderDiff(calculatedDiff));
+  const { render } = getRender(format);
+  return render(calculatedDiff);
 };
